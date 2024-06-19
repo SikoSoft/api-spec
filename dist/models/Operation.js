13400ea69d984e9d46f67f02327b67ed595d2149
Aaron Wright <aaron@sikosoft.com>
2024-06-19 22:10:59 +0200

    1.2.1 add missing dist files for operation

---
+++ dist/models/Operation.js
@@ -0,0 +1,7 @@
+export var OperationType;
+(function (OperationType) {
+    OperationType["REPLACE_TAGS"] = "replaceTags";
+    OperationType["ADD_TAGS"] = "addTags";
+    OperationType["REMOVE_TAGS"] = "removeTags";
+})(OperationType || (OperationType = {}));
+//# sourceMappingURL=Operation.js.map
