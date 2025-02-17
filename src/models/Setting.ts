bf940ca6208987b2cdd193863d3bb6f01d97f25f
Aaron Wright <aaron@sikosoft.com>
2025-02-17 18:12:49 +0100

    cleanup in Setting interfaces

--- src/models/Setting.ts
+++ src/models/Setting.ts
@@ -16,23 +16,6 @@ export interface SettingConfig {
   [SettingName.PAGINATION_PAGE_SIZE]: SettingType.NUMBER;
 }
 
-export interface Setting<SettingName> {
-  name: SettingName;
-  type: SettingType;
-}
-
-export interface ToggleSetting extends Setting {
-  value: boolean;
-}
-
-export interface NumberSetting extends Setting {
-  value: number;
-}
-
-export interface TextSetting extends Setting {
-  value: string;
-}
-
 export interface Settings {
   listConfig: ListConfig;
   settings: { [Property in keyof SettingConfig]: SettingConfig[Property] };
