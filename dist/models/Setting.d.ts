88bacb93dd6485ecb314a4154e99dda85d4707f0
Aaron Wright <aaron@sikosoft.com>
2025-02-18 20:39:57 +0100

    1.8.0 Setting model updates

--- dist/models/Setting.d.ts
+++ dist/models/Setting.d.ts
@@ -1,23 +1,44 @@
-import { ListConfig } from "./List";
 export declare enum SettingType {
     TOGGLE = "toggle",
     NUMBER = "number",
     TEXT = "text"
 }
-export interface Setting {
-    name: string;
-    type: SettingType;
+export interface SettingTypeConfig {
+    [SettingType.TOGGLE]: boolean;
+    [SettingType.NUMBER]: number;
+    [SettingType.TEXT]: string;
 }
-export interface ToggleSetting extends Setting {
-    value: boolean;
+export declare enum SettingName {
+    PAGINATION_TYPE = "paginationType",
+    PAGINATION_PAGE_SIZE = "paginationPageSize"
 }
-export interface NumberSetting extends Setting {
-    value: number;
+export declare enum PaginationType {
+    LAZY = "lazy",
+    NAVIGATION = "navigation"
 }
-export interface TextSetting extends Setting {
-    value: string;
+export interface SettingConfig {
+    valueType: any;
+    controlType: SettingType;
 }
-export interface Settings {
-    listConfig: ListConfig;
-    settings: Setting[];
-}
+export type PaginationIndex = keyof typeof PaginationType;
+export type PaginationValue = (typeof PaginationType)[PaginationIndex];
+export type SettingsConfig = {
+    [SettingName.PAGINATION_TYPE]: {
+        value: PaginationType;
+        control: SettingType.TEXT;
+    };
+    [SettingName.PAGINATION_PAGE_SIZE]: {
+        value: number;
+        control: SettingType.NUMBER;
+    };
+};
+export type Setting = {
+    [K in keyof SettingsConfig]: {
+        name: K;
+        value: SettingsConfig[K];
+    };
+}[keyof SettingsConfig];
+export type Settings = {
+    [Property in keyof SettingsConfig]: SettingsConfig[Property]["value"];
+};
+export declare const defaultSettings: Settings;
--- dist/models/Setting.js
+++ dist/models/Setting.js
@@ -4,4 +4,18 @@ export var SettingType;
     SettingType["NUMBER"] = "number";
     SettingType["TEXT"] = "text";
 })(SettingType || (SettingType = {}));
+export var SettingName;
+(function (SettingName) {
+    SettingName["PAGINATION_TYPE"] = "paginationType";
+    SettingName["PAGINATION_PAGE_SIZE"] = "paginationPageSize";
+})(SettingName || (SettingName = {}));
+export var PaginationType;
+(function (PaginationType) {
+    PaginationType["LAZY"] = "lazy";
+    PaginationType["NAVIGATION"] = "navigation";
+})(PaginationType || (PaginationType = {}));
+export const defaultSettings = {
+    paginationType: PaginationType.LAZY,
+    paginationPageSize: 10,
+};
 //# sourceMappingURL=Setting.js.map
