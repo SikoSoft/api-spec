1387ad6668d20baec8f86b001923e12f54faeab4
Aaron Wright <aaron@sikosoft.com>
2025-03-07 20:24:57 +0100

    add Property models

---
+++ src/models/Property.ts
@@ -0,0 +1,20 @@
+export enum PropertyDataType {
+  TEXT = "text",
+  NUMBER = "number",
+  BOOLEAN = "boolean",
+}
+
+export enum PropertyControlType {
+  TEXT = "text",
+  NUMBER = "number",
+  BOOLEAN = "boolean",
+  SELECT = "select",
+}
+
+export enum PropertyRenderType {
+  PLAIN_TEXT = "plainText",
+  RICH_TEXT = "richText",
+  IMAGE = "image",
+  VIDEO = "video",
+  HIDDEN = "hidden",
+}
