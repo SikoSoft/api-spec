98e120e7843a713dc08c22dfba9949c45bd28029
Aaron Wright <aaron@sikosoft.com>
2025-03-07 20:30:42 +0100

    add dist Property files

---
+++ dist/models/Property.d.ts
@@ -0,0 +1,18 @@
+export declare enum PropertyDataType {
+    TEXT = "text",
+    NUMBER = "number",
+    BOOLEAN = "boolean"
+}
+export declare enum PropertyControlType {
+    TEXT = "text",
+    NUMBER = "number",
+    BOOLEAN = "boolean",
+    SELECT = "select"
+}
+export declare enum PropertyRenderType {
+    PLAIN_TEXT = "plainText",
+    RICH_TEXT = "richText",
+    IMAGE = "image",
+    VIDEO = "video",
+    HIDDEN = "hidden"
+}
---
+++ dist/models/Property.js
@@ -0,0 +1,22 @@
+export var PropertyDataType;
+(function (PropertyDataType) {
+    PropertyDataType["TEXT"] = "text";
+    PropertyDataType["NUMBER"] = "number";
+    PropertyDataType["BOOLEAN"] = "boolean";
+})(PropertyDataType || (PropertyDataType = {}));
+export var PropertyControlType;
+(function (PropertyControlType) {
+    PropertyControlType["TEXT"] = "text";
+    PropertyControlType["NUMBER"] = "number";
+    PropertyControlType["BOOLEAN"] = "boolean";
+    PropertyControlType["SELECT"] = "select";
+})(PropertyControlType || (PropertyControlType = {}));
+export var PropertyRenderType;
+(function (PropertyRenderType) {
+    PropertyRenderType["PLAIN_TEXT"] = "plainText";
+    PropertyRenderType["RICH_TEXT"] = "richText";
+    PropertyRenderType["IMAGE"] = "image";
+    PropertyRenderType["VIDEO"] = "video";
+    PropertyRenderType["HIDDEN"] = "hidden";
+})(PropertyRenderType || (PropertyRenderType = {}));
+//# sourceMappingURL=Property.js.map
