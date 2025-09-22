a87ffe0aee966d56539d42d6ffcff40c282bd485
Aaron Wright <aaron@sikosoft.com>
2025-09-22 22:55:12 +0200

    update Entity dist file

--- dist/models/Entity.d.ts
+++ dist/models/Entity.d.ts
@@ -103,8 +103,11 @@ export interface EntityProperty {
 }
 export interface Entity {
     id: number;
-    name: string;
-    properties: PropertyConfig[];
+    type: number;
+    createdAt: string;
+    updatedAt: string;
+    tags: string[];
+    properties: EntityProperty[];
 }
 export interface CommonProperty {
     id: number;
