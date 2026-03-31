1783977625966c62eff58a9d47d7187bae637e87
Aaron Wright <aaron@sikosoft.com>
2026-03-31 16:20:59 +0200

    add ListConfig library to provide utility functions, entitySatisfiesFilter for now

---
+++ src/lib/ListConfig.ts
@@ -0,0 +1,43 @@
+import { Entity } from "../models/Entity";
+import { ListFilter } from "../models/List";
+
+export class ListConfig {
+  static entitySatisfiesFilter(
+    entity: Entity,
+    listFilter: ListFilter
+  ): boolean {
+    if (listFilter.includeAll) {
+      return true;
+    }
+
+    if (
+      listFilter.includeTypes.length > 0 &&
+      !listFilter.includeTypes.includes(entity.type)
+    ) {
+      return false;
+    }
+
+    if (!listFilter.includeAllTagging) {
+      if (!(listFilter.includeUntagged && entity.tags.length === 0)) {
+        if (
+          !listFilter.tagging.containsAllOf.every((tag) =>
+            entity.tags.includes(tag)
+          )
+        ) {
+          return false;
+        }
+
+        if (
+          listFilter.tagging.containsOneOf.length > 0 &&
+          !listFilter.tagging.containsOneOf.some((tag) =>
+            entity.tags.includes(tag)
+          )
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
