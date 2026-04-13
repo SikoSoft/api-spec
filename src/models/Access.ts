109d682363fc43d17306c6832caa942b6b157da7
Aaron Wright <aaron@sikosoft.com>
2026-04-13 20:26:52 +0200

    add Access models

---
+++ src/models/Access.ts
@@ -0,0 +1,17 @@
+export enum AccessType {
+  USER = "user",
+  GROUP = "group",
+}
+
+export interface AccessRule {
+  id: number;
+  type: AccessType;
+  targetId: string;
+}
+
+export interface AccessPolicy {
+  id: number;
+  name: string;
+  description: string;
+  accessRules: AccessRule[];
+}
