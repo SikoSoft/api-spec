3c501db00f34dee8feb498cf159e39bf2dd6510d
Aaron Wright <aaron@sikosoft.com>
2026-04-29 14:29:27 +0200

    1.74.0 added operation to FilterProperty interface

--- dist/models/List.d.ts
+++ dist/models/List.d.ts
@@ -37,6 +37,7 @@ export type TaggingContext = Record<ListFilterType, string[]>;
 export interface FilterProperty {
     propertyId: number;
     value: PropertyDataValue;
+    operation: TextType;
 }
 export interface ListFilter {
     tagging: TaggingContext;
