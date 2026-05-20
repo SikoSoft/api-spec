e97440417eaa0c55bb11cb670532ac1631daf34f
Aaron Wright <aaron@sikosoft.com>
2026-05-20 22:15:02 +0200

    1.85.0 added params to Critereon interface

--- dist/models/Medal.d.ts
+++ dist/models/Medal.d.ts
@@ -1,12 +1,15 @@
-export interface MedalCriteria {
-    id: number;
-    medalId: number;
-    type: string;
-    value: string;
-    createdAt: string;
-    updatedAt: string;
+export type EvalOperator = "==" | "!=" | ">" | ">=" | "<" | "<=" | "contains";
+export interface Criterion {
+    fact: string;
+    operator: EvalOperator;
+    value: string | number | boolean | string[];
+    params?: Record<string, unknown>;
 }
-export interface Medal {
+export interface Criteria {
+    any?: (Criterion | Criteria)[];
+    all?: (Criterion | Criteria)[];
+}
+export interface MedalConfig {
     id: number;
     name: string;
     description: string;
@@ -16,5 +19,11 @@ export interface Medal {
     icon: string;
     createdAt: string;
     updatedAt: string;
-    criteria: MedalCriteria[];
+    criteria: Criterion | Criteria;
+}
+export interface Medal {
+    id: number;
+    userId: string;
+    medalConfigId: number;
+    awardedAt: string;
 }
--- dist/models/index.js
+++ dist/models/index.js
@@ -1,11 +1,14 @@
 export * as Access from "./Access";
 export * as Data from "./Data";
 export * as Entity from "./Entity";
+export * as Hook from "./Hook";
 export * as Identity from "./Identity";
 export * as Introspection from "./Introspection";
 export * as List from "./List";
+export * as Medal from "./Medal";
 export * as Operation from "./Operation";
 export * as Revision from "./Revision";
 export * as Setting from "./Setting";
+export * as Statistic from "./Statistic";
 export * as Workspace from "./Workspace";
 //# sourceMappingURL=index.js.map
