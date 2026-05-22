0d7830aa00e26fd351205ef6b4ab3c77bfff95fd
Aaron Wright <aaron@sikosoft.com>
2026-05-22 06:52:40 +0200

    1.86.0 added FactRequest to Medal models

--- dist/models/Medal.d.ts
+++ dist/models/Medal.d.ts
@@ -1,13 +1,36 @@
+import { ListFilter } from "./List";
 export type EvalOperator = "==" | "!=" | ">" | ">=" | "<" | "<=" | "contains";
 export interface Criterion {
     fact: string;
     operator: EvalOperator;
     value: string | number | boolean | string[];
-    params?: Record<string, unknown>;
 }
 export interface Criteria {
     any?: (Criterion | Criteria)[];
     all?: (Criterion | Criteria)[];
+}
+export declare enum FactOperation {
+    ENTITY_COUNT = "entityCount",
+    UNIQUE_TAG_COUNT = "uniqueTagCount",
+    MEDAL_COUNT = "medalCount"
+}
+export type EntityCountFactContext = {
+    operation: FactOperation.ENTITY_COUNT;
+    filter: ListFilter;
+};
+export type UniqueTagCountFactContext = {
+    operation: FactOperation.UNIQUE_TAG_COUNT;
+    filter: ListFilter;
+};
+export type MedalCountFactContext = {
+    operation: FactOperation.MEDAL_COUNT;
+    medalConfigId: number;
+    series: string;
+};
+export type FactContext = EntityCountFactContext | UniqueTagCountFactContext | MedalCountFactContext;
+export interface FactRequest {
+    alias: string;
+    context: FactContext;
 }
 export interface MedalConfig {
     id: number;
@@ -19,6 +42,7 @@ export interface MedalConfig {
     icon: string;
     createdAt: string;
     updatedAt: string;
+    factRequests: FactRequest[];
     criteria: Criterion | Criteria;
 }
 export interface Medal {
--- dist/models/Medal.js
+++ dist/models/Medal.js
@@ -1,2 +1,7 @@
-export {};
+export var FactOperation;
+(function (FactOperation) {
+    FactOperation["ENTITY_COUNT"] = "entityCount";
+    FactOperation["UNIQUE_TAG_COUNT"] = "uniqueTagCount";
+    FactOperation["MEDAL_COUNT"] = "medalCount";
+})(FactOperation || (FactOperation = {}));
 //# sourceMappingURL=Medal.js.map
