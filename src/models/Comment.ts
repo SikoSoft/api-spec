1d7212be87b65ecd1f0d6d78573832cc18cd5cfa
Aaron Wright <aaron@sikosoft.com>
2026-05-26 15:08:20 +0200

    add Comment models

---
+++ src/models/Comment.ts
@@ -0,0 +1,18 @@
+import { User } from "./Identity";
+
+export interface CommonComment {
+  id: number;
+  entityId: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  user: User | null;
+}
+
+export interface AnonymousComment extends CommonComment {
+  user: null;
+}
+
+export interface UserComment extends CommonComment {
+  user: User;
+}
